f2d2a73ad4d648dbafaf50121040ee14ac0192fe
aniljharman <anil.jose@harman.com>
2025-05-29 22:23:29 +0530

    Initial commit for AKS Terraform setup

--- terraform.tfvars
+++ terraform.tfvars
@@ -2,3 +2,8 @@ resource_group_name = "aks-lab-rg"
 location            = "eastus"
 aks_cluster_name    = "aks-lab"
 node_count          = 2
+subscription_id = "dummy"
+client_id       = "dummy"
+client_secret   = "dummy"
+tenant_id       = "dummy"
+
--- variables.tf
+++ variables.tf
@@ -1,19 +1,20 @@
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+
 variable "resource_group_name" {
-  type    = string
   default = "aks-lab-rg"
 }
 
 variable "location" {
-  type    = string
   default = "eastus"
 }
 
 variable "aks_cluster_name" {
-  type    = string
   default = "aks-lab"
 }
 
 variable "node_count" {
-  type    = number
   default = 2
 }
